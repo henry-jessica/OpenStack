61a0ef5c8c49cceb36e9ad74a7faac0ebf7c61d7
jessica <jessicahenry2604@gmail.com>
2022-11-27 13:24:08 +0000

    before unit tests

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,33 +1,37 @@
 import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { EventDetailsComponent } from '../event-details/event-details.component';
-
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+  styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent implements OnInit {
-  
-  @Output()deleteConfirmed = new EventEmitter<boolean>();
+  @Output() deleteConfirmed = new EventEmitter<boolean>();
 
-  constructor(public dialogRef: MatDialogRef<MessagesComponent>, private dialog:MatDialog, public dialogRef2: MatDialogRef<EventDetailsComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(
+    public dialogRef: MatDialogRef<MessagesComponent>,
+    private dialog: MatDialog,
+    public dialogRef2: MatDialogRef<EventDetailsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
     console.log('message', this.data);
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  doNotCancel() {
+    this.dialogRef.close();
   }
+  onCancel() {
+    this.dialog.closeAll();
 
-doNotCancel(){
-  this.dialogRef.close();  
-
-}
-onCancel(){
-  this.dialog.closeAll();  
-
-  if(this.data.message.taskName='Delete'){
-    this.dialogRef.close({option:'deleteConfirmed'}); 
+    if ((this.data.message.taskName = 'Delete')) {
+      this.dialogRef.close({ option: 'deleteConfirmed' });
+    }
   }
 }
-}
-
