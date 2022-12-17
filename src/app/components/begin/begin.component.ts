80f7bcac70ec261bc7a2377e35c3649e9e9171e3
jessica <jessicahenry2604@gmail.com>
2022-12-17 16:39:12 +0000

    get user role

--- src/app/components/begin/begin.component.ts
+++ src/app/components/begin/begin.component.ts
@@ -3,28 +3,51 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 // Import the AuthService type from the SDK
 import { AuthService } from '@auth0/auth0-angular';
+import { AuthService as AuthAPIService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-begin',
   templateUrl: './begin.component.html',
-  styleUrls: ['./begin.component.scss']
+  styleUrls: ['./begin.component.scss'],
 })
 export class BeginComponent implements OnInit {
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public auth: AuthService,
+    private router: Router,
+    private _httpAuthService: AuthAPIService, 
 
-  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService, private router:Router){}
+  ) {}
 
-  isAuthenticated$ = this.auth.isAuthenticated$
+  isAuthenticated$ = this.auth.isAuthenticated$;
 
   ngOnInit(): void {
-    this.auth.isAuthenticated$.subscribe(isAuthenticated =>{
-      if(isAuthenticated){
-        this.router.navigate(['/home'])
-      }
-    })
+    // this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+    //   if (isAuthenticated) {
+    //     this.router.navigate(['/home']);
+    //   }
+    // })
+
+    this.getUserRole();
   }
 
-  login(){
-    this.auth.loginWithRedirect(); 
+  getUserRole() {
+    console.log('CALLED  ');
+
+    this.auth.user$.subscribe((user) => {
+      console.log('USER ==> ', user?.sub);
+      console.log('this.userId', user?.sub);
+      const res =
+        user?.sub &&
+        this._httpAuthService.getUserRole(user?.sub).subscribe((res) => {
+          console.log('USER ROLE: ', res[0].name);
+          // navigate to home screen
+          this.router.navigate(['/home']);
+        });
+    });
   }
 
+  login() {
+    this.auth.loginWithRedirect();
+  }
 }
