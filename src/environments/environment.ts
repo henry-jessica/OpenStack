e4b065943c9ad815ce3f0ff41e691ec0a526bbca
jessica <jessicahenry2604@gmail.com>
2022-12-19 16:42:55 +0000

    domain henry available - updated!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUri: 'http://localhost:3000',
+  apiUri: 'https://henrysolutions.ie/',
   auth0: {
     domain: 'dev-zxkcix0u0cipypz8.eu.auth0.com',
     clientId: 'tGDugZvGb9UhwjJIBajPGqhcJEPuh09E',
