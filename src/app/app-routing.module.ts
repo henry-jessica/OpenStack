ac4edde0d7966b5052557789ad6664d9601c4c99
jessica <jessicahenry2604@gmail.com>
2023-03-10 20:32:07 +0000

    cleaning the app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BeginComponent } from './components/begin/begin.component';
 import { EventDetailsComponent } from './components/event-details/event-details.component';
 import {HomeComponent} from './components/home/home.component'; 
+import { LoginComponent } from './login/login.component';
 // import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
@@ -14,6 +15,7 @@ const routes: Routes = [
   // { path: 'begin', component: BeginComponent },
   { path: '**', component: HomeComponent },
   // { path: 'home', component: BeginComponent, canActivate:[AuthGuard] },
+  { path:'login', component:LoginComponent},
 
 
 ];
