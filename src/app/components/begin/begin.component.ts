e482821495255b2c7652eb05d805057cab082a69
jessica <jessicahenry2604@gmail.com>
2023-03-10 21:42:56 +0000

    fix bug - cognito now is working

--- src/app/components/begin/begin.component.ts
+++ src/app/components/begin/begin.component.ts
@@ -2,7 +2,7 @@ import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 // Import the AuthService type from the SDK
-import { AuthService } from '@auth0/auth0-angular';
+// import { AuthService } from '@auth0/auth0-angular';
 import { Store } from '@ngxs/store';
 import { AddAuth } from 'app/store/auth.actions';
 import { AuthService as AuthAPIService } from '../../services/auth.service';
@@ -15,13 +15,13 @@ import { AuthService as AuthAPIService } from '../../services/auth.service';
 export class BeginComponent implements OnInit {
   constructor(
     @Inject(DOCUMENT) public document: Document,
-    public auth: AuthService,
+    // public auth: AuthService,
     private router: Router,
-    private _httpEventService: AuthAPIService,
-    private store: Store
+    // private _httpEventService: AuthAPIService,
+    // private store: Store
   ) {}
 
-  isAuthenticated$ = this.auth.isAuthenticated$;
+  // isAuthenticated$ = this.auth.isAuthenticated$;
 
   ngOnInit(): void {
     // this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
@@ -33,32 +33,32 @@ export class BeginComponent implements OnInit {
     // this.getUserRole();
   }
 
-  getUserRole() {
-    console.log('CALLED  ');
+  // getUserRole() {
+  //   console.log('CALLED  ');
 
-    this.auth.user$.subscribe((user) => {
-      console.log('USER ==> ', user?.sub);
-      console.log('this.userId', user?.sub);
-      if(user?.sub){
-        this.router.navigate(['/home']);
-      }
-      const res =
-        user?.sub &&
-        this._httpEventService.getUserRole(user?.sub).subscribe((res) => {
-          console.log('USER ROLE: ', res[0]?.name);
-          this.store.dispatch(
-            new AddAuth({
-              id: res[0]?.id,
-              name: res[0]?.name,
-              description: res[0]?.description,
-            })
-          );
+  //   this.auth.user$.subscribe((user) => {
+  //     console.log('USER ==> ', user?.sub);
+  //     console.log('this.userId', user?.sub);
+  //     if(user?.sub){
+  //       this.router.navigate(['/home']);
+  //     }
+  //     const res =
+  //       user?.sub &&
+  //       this._httpEventService.getUserRole(user?.sub).subscribe((res) => {
+  //         console.log('USER ROLE: ', res[0]?.name);
+  //         this.store.dispatch(
+  //           new AddAuth({
+  //             id: res[0]?.id,
+  //             name: res[0]?.name,
+  //             description: res[0]?.description,
+  //           })
+  //         );
 
-        });
-    });
-  }
+  //       });
+  //   });
+  // }
 
-  login() {
-    this.auth.loginWithRedirect();
-  }
+  // login() {
+  //   this.auth.loginWithRedirect();
+  // }
 }
