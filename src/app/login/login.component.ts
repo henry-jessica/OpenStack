c63c836b2c71469883b6ad31024d943f5685845e
jessica <jessicahenry2604@gmail.com>
2023-03-10 22:18:37 +0000

    TODO: Config google api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticatorService } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( public authenticator: AuthenticatorService ) { }
 
   ngOnInit(): void {
   }
