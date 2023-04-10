18702d01c6f51d6f00a00ec89107de46d7608498
jessica <jessicahenry2604@gmail.com>
2023-04-10 21:05:16 +0100

    nav 1 is dome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,8 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: 'events/:id', component: EventDetailsComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: LoginComponent },
+
   { path: '#', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'home/evens/:id', component: EventDetailsComponent },
